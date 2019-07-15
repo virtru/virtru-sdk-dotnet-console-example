72391aa9dbd83eda808d2c815a0620afcbf45a51
jgilpin <jgilpin@virtru.com>
2019-07-15 10:49:35 -0400

    Add project files.

---
+++ virtru-sdk-dotnet-console-example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtru_sdk_dotnet_console_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
